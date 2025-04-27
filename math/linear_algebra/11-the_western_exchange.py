2057dd61020305041b9fa62837f436d899d008ef
Elyaaa001 <gabibovaelina75@gmail.com>
2025-04-27 18:41:21 +0400

    tasks done

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''this function returns a transposed matrix'''
+
+
+def np_transpose(matrix):
+    '''This function returns a transposed matrix'''
+    return matrix.transpose()

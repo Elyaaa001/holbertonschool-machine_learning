d331b8febddbbb7b6ac36641d43284ae233c114f
Elyaaa001 <gabibovaelina75@gmail.com>
2025-05-04 23:50:12 +0400

    some errors

--- supervised_learning/keras/13-predict.py
+++ supervised_learning/keras/13-predict.py
@@ -1,30 +1,18 @@
 #!/usr/bin/env python3
 
-import tensorflow.keras as K
-
-def one_hot(Y, classes):
-    return K.utils.to_categorical(Y, num_classes=classes)
-
-def load_model(path):
-    return K.models.load_model(path)
-
-def predict(network, data):
-    return network.predict(data)
-
-def reshape(data):
-    return data.reshape((data.shape[0], -1))
-
-def argmax(predictions):
-    return K.backend.eval(K.backend.argmax(predictions, axis=1))
+import numpy as np
+one_hot = __import__('3-one_hot').one_hot
+load_model = __import__('9-model').load_model
+predict = __import__('13-predict').predict
 
 if __name__ == '__main__':
-
-    (X_train, Y_train), (X_test, Y_test) = K.datasets.mnist.load_data()
-    X_test = reshape(X_test.astype('float32') / 255)
+    datasets = np.load('MNIST.npz')
+    X_test = datasets['X_test']
+    X_test = X_test.reshape(X_test.shape[0], -1)
+    Y_test = datasets['Y_test']
 
     network = load_model('network2.keras')
     Y_pred = predict(network, X_test)
 
     print(Y_pred)
-    print(argmax(Y_pred))
-    print(Y_test)
+    print(np.argmax(Y_pred, axis=1))  # ✅ Matches desired output

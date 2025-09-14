f02722db447bc1268fef68ed8737a187775c82e1
Elyaaa001 <gabibovaelina75@gmail.com>
2025-09-14 17:40:43 +0400

    tasks

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -1,38 +1,24 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May 11 08:12:42 2021
-
-@author: Robinson Montes
-"""
+'''creates a bag of words embedding matrix
+https://scikit-learn.org/stable/modules/feature_extraction.html
+https://medium.com/@arihantjain25121995/word-embeddings-using-bow-tf-idf-with-
+an-example-a10d2e2ab03e
+'''
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def tf_idf(sentences, vocab=None):
-    """
-    Function that creates a TF-IDF embedding
-
-    Arguments:
-     - sentences is a list of sentences to analyze
-     - vocab is a list of the vocabulary words to use for the analysis
-        * If None, all words within sentences should be used
-
-    Returns:
-     embeddings, features
-        - embeddings is a numpy.ndarray of shape (s, f)
-            containing the embeddings
-            * s is the number of sentences in sentences
-            * f is the number of features analyzed
-        - features is a list of the features used for embeddings
-    """
-    if vocab is None:
-        vectorizer = TfidfVectorizer()
-        X = vectorizer.fit_transform(sentences)
-        vocab = vectorizer.get_feature_names()
-    else:
-        vectorizer = TfidfVectorizer(vocabulary=vocab)
-        X = vectorizer.fit_transform(sentences)
-
-    embedding = X.toarray()
-
-    return embedding, vocab
+    '''creates a bag of words embedding matrix:
+    Args:
+        sentences is a list of sentences to analyze
+        vocab is a list of the vocabulary words to use for the analysis
+            If None, all words within sentences should be used
+    Returns: embeddings, features
+        embeddings is a numpy.ndarray of shape (s, f) containing the embeddings
+            s is the number of sentences in sentences
+            f is the number of features analyzed
+        features is a list of the features used for embeddings
+    '''
+    vectorizer = TfidfVectorizer(vocabulary=vocab)
+    X = vectorizer.fit_transform(sentences)
+    return (X.toarray(), vectorizer.get_feature_names())
--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -1,24 +1,13 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May 11 08:12:42 2021
-
-@author: Robinson Montes
-"""
-import tensorflow.keras as K
+'''converts a gensim word2vec model to a keras
+'''
 from gensim.models import Word2Vec
 
 
 def gensim_to_keras(model):
-    """
-    Function that converts a gensim word2vec model to a keras Embedding layer
-
-    Arguments:
-     - model is a trained gensim word2vec models
-
-    Returns:
-     The trainable keras Embedding
-    Note: you need keras=2.2.2 to run get_keras_embedding()
-    """
-
-    return model.wv.get_keras_embedding(train_embeddings=True)
+    '''converts a gensim word2vec model to a keras
+    Args:
+        model is a trained gensim word2vec models
+    Returns: the trainable keras Embedding
+    '''
+    return model.wv.get_keras_embedding()

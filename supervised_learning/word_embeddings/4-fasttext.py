4177d1ffa6e23b6940e8c4f06222637b5d7c90d7
Elyaaa001 <gabibovaelina75@gmail.com>
2025-09-22 12:07:09 +0400

    tasks

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -1,32 +1,23 @@
 #!/usr/bin/env python3
-'''creates and trains a genism fastText model
-https://radimrehurek.com/gensim/models/fasttext.html
-'''
+"""
+A function that creates and trains a genism fastText model
+"""
+from gensim.test.utils import common_texts, get_tmpfile
 from gensim.models import FastText
 
 
 def fasttext_model(sentences, size=100, min_count=5, negative=5, window=5,
                    cbow=True, iterations=5, seed=0, workers=1):
-    '''creates and trains a genism fastText model
-    Args:
-        sentences is a list of sentences to be trained on
-        size is the dimensionality of the embedding layer
-        min_count is the minimum number of occurrences of a word for use
-                  in training
-        window is the maximum distance between the current and predicted
-               word within a sentence
-        negative is the size of negative sampling
-        cbow is a boolean to determine the training type; True is for CBOW
-            False is for Skip-gram
-        iterations is the number of iterations to train over
-        seed is the seed for the random number generator
-        workers is the number of worker threads to train the model
-    Returns: the trained model
-    '''
-    model = FastText(sentences=sentences, size=size, window=window,
-                     min_count=min_count, workers=workers, seed=seed,
-                     sg=cbow, iter=iterations,
-                     negative=negative)
-    model.train(sentences=sentences, total_examples=model.corpus_count,
-                epochs=model.epochs)
+    """
+    A function that creates and trains a genism fastText model 
+    """
+    if cbow is True:
+        skip = 0
+    else:
+        skip = 1
+    model = FastText(size=size, window=window,
+                     min_count=min_count, workers=workers, sg=skip,
+                     negative=negative, seed=seed)
+    model.build_vocab(sentences)
+    model.train(sentences, total_examples=model.corpus_count, epochs=iterations)
     return model

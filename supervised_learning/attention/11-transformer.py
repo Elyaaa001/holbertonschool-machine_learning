2af006e0af3833d53d887b72285d178f152a0f22
Elyaaa001 <gabibovaelina75@gmail.com>
2025-10-16 11:07:40 +0400

    tasks

--- supervised_learning/attention/11-transformer.py
+++ supervised_learning/attention/11-transformer.py
@@ -1,32 +1,29 @@
 #!/usr/bin/env python3
-'''class Transformer
-https://www.tensorflow.org/tutorials/text/transformer#create_the_transformer
-'''
+'''Transformer class using TensorFlow Keras'''
+
 import tensorflow as tf
 Encoder = __import__('9-transformer_encoder').Encoder
 Decoder = __import__('10-transformer_decoder').Decoder
 
-
 class Transformer(tf.keras.Model):
-    '''class Transformer'''
+    """Transformer network"""
     def __init__(self, N, dm, h, hidden, input_vocab, target_vocab,
                  max_seq_input, max_seq_target, drop_rate=0.1):
-        '''Constructor
-        @params
-            N - the number of blocks in the encoder and decoder
-            dm - the dimensionality of the model
-            h - the number of heads
-            hidden - the number of hidden units in the fully connected layers
-            input_vocab - the size of the input vocabulary
-            target_vocab - the size of the target vocabulary
-            max_seq_input - the maximum sequence length possible for the input
-            max_seq_target: the maximum sequence length possible for the target
-            drop_rate - the dropout rate
-        Sets the following public instance attributes:
-            encoder - the encoder layer
-            decoder - the decoder layer
-            linear - a final Dense layer with target_vocab units
-        '''
+        """
+        Constructor
+        Args:
+            N: number of blocks in encoder and decoder
+            dm: model dimensionality
+            h: number of heads
+            hidden: hidden units in feed-forward layers
+            input_vocab: input vocabulary size
+            target_vocab: target vocabulary size
+            max_seq_input: max input sequence length
+            max_seq_target: max target sequence length
+            drop_rate: dropout rate
+        Public attributes:
+            encoder, decoder, linear
+        """
         super(Transformer, self).__init__()
 
         self.encoder = Encoder(N, dm, h, hidden,
@@ -39,24 +36,57 @@ class Transformer(tf.keras.Model):
 
     def call(self, inputs, target, training, encoder_mask, look_ahead_mask,
              decoder_mask):
-        '''call method
+        """
+        Forward pass
         Args:
-            inputs:tensor of shape (batch, input_seq_len)containing the inputs
-            target:tensor of shape (batch, target_seq_len)containing the target
-            training - a boolean to determine if the model is training
-            encoder_mask - the padding mask to be applied to the encoder
-            look_ahead_mask - the look ahead mask to be applied to the decoder
-            decoder_mask - the padding mask to be applied to the decoder
-        Returns: a tensor of shape (batch, target_seq_len, target_vocab)
-                containing the transformer output
-        '''
-        # (batch_size, inp_seq_len, dm)
+            inputs: tensor (batch, input_seq_len)
+            target: tensor (batch, target_seq_len)
+            training: boolean
+            encoder_mask: padding mask for encoder
+            look_ahead_mask: look-ahead mask for decoder
+            decoder_mask: padding mask for decoder
+        Returns:
+            tensor (batch, target_seq_len, target_vocab)
+        """
+        # Encoder output
         enc_output = self.encoder(inputs, training, encoder_mask)
 
-        # dec_output.shape == (batch_size, tar_seq_len, dm)
-        dec_output, attention_weights = self.decoder(
-            target, enc_output, training, look_ahead_mask, decoder_mask)
-        # (batch_size, tar_seq_len, target_vocab_size)
+        # Decoder output
+        dec_output, _ = self.decoder(target, enc_output, training,
+                                     look_ahead_mask, decoder_mask)
+
+        # Final linear layer
         final_output = self.linear(dec_output)
 
         return final_output
+
+
+if __name__ == "__main__":
+    # Set seeds for reproducibility
+    import os, random, numpy as np
+    SEED = 0
+    os.environ['PYTHONHASHSEED'] = str(SEED)
+    os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+    random.seed(SEED)
+    np.random.seed(SEED)
+    tf.random.set_seed(SEED)
+
+    # Instantiate transformer
+    transformer = Transformer(6, 512, 8, 2048, 10000, 12000, 1000, 1500)
+
+    # Print encoder, decoder, and final layer
+    print(transformer.encoder)
+    print(transformer.decoder)
+    print(transformer.linear)
+
+    # Fake input tensors
+    x = tf.random.uniform((32, 10), dtype=tf.float32)
+    y = tf.random.uniform((32, 15), dtype=tf.float32)
+
+    # Forward pass
+    output = transformer(x, y, training=True, encoder_mask=None,
+                         look_ahead_mask=None, decoder_mask=None)
+
+    # Print output shape and tensor
+    print(output.shape)
+    print(output)

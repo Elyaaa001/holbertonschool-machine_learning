6549b445c527023bd061a033fd4fbb517ffc17bd
Elyaaa001 <gabibovaelina75@gmail.com>
2025-10-23 14:57:21 +0400

    task

--- reinforcement_learning/q_learning/1-q_init.py
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -1,18 +1,37 @@
 #!/usr/bin/env python3
-""" task 1: 1. Initialize the Q-table"""
-import numpy as np
+"""Load the FrozenLake environment from Gymnasium."""
+
+import gymnasium as gym
 
 
-def q_init(env):
-    """[initializes the Q-table:]
-    Args:
-        env ([type]): [description]
-    Returns: the Q-table as a numpy.ndarray of zeros
+def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
     """
+    Load the pre-made FrozenLake environment from Gymnasium.
 
-    action_space_size = env.action_space.n
-    states_space_size = env.observation_space.n
+    Args:
+        desc: either None or a list of lists containing a custom
+              description of the map to load for the environment.
+        map_name: either None or a string containing the pre-made
+                  map to load.  If both desc and map_name are None,
+                  a randomly generated 8x8 map is used.
+        is_slippery: boolean to determine if the ice is slippery.
 
-    q_table = np.zeros((states_space_size, action_space_size))
+    Returns:
+        The environment.
+    """
+    if desc is None and map_name is None:
+        map_name = "8x8"
 
-    return q_table
+    if desc is not None:
+        env = gym.make(
+            "FrozenLake-v1",
+            desc=desc,
+            is_slippery=is_slippery
+        )
+    else:
+        env = gym.make(
+            "FrozenLake-v1",
+            map_name=map_name,
+            is_slippery=is_slippery
+        )
+    return env

4dbc2a41e90913616d07fae4b0c876eb0f9e0804
Elyaaa001 <gabibovaelina75@gmail.com>
2025-10-23 15:05:13 +0400

    task

--- reinforcement_learning/q_learning/2-epsilon_greedy.py
+++ reinforcement_learning/q_learning/2-epsilon_greedy.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
-""" Task 2: 2. Epsilon Greedy """
+"""
+Epsilon-greedy action selection for a given state.
+"""
+
 import numpy as np
 
 
 def epsilon_greedy(Q, state, epsilon):
     """
-        Selects the next action using the epsilon-greedy policy.
+    Uses epsilon-greedy to determine the next action.
 
     Args:
-        Q (numpy.ndarray): Q-table of shape (states, actions)
-        state (int): current state
-        epsilon (float): probability of choosing a random action (exploration)
+        Q (numpy.ndarray): Q-table.
+        state (int): current state.
+        epsilon (float): probability of exploring.
 
     Returns:
-        int: index of the action selected
+        int: the index of the next action.
     """
+    p = np.random.uniform(0, 1)
 
-    # https://www.youtube.com/watch?v=HGeI30uATws&list=PLZbbT5o_s2xoWNVdDudn51XM8lOuZ_Njv&index=9
-    # ε = epsilon
-    # print(Q)
-    # print(Q.shape)
-    # print(Q[state])
-    # print (state, epsilon)
-
-    if np.random.uniform(0, 1) < epsilon:
-        return int(np.random.randint(Q.shape[1]))
+    # Explore: choose random action
+    if p < epsilon:
+        action = np.random.randint(Q.shape[1])
+    # Exploit: choose best known action
     else:
-        return int(np.argmax(Q[state]))
+        action = np.argmax(Q[state])
+
+    return action
